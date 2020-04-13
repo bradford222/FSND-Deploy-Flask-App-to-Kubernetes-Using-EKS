2a9905255359e58a0bd36394af2e4858f6195ce7
Brad Schneider <brad@adaptivemgmt.com>
2020-04-13 18:09:07 -0400

    More small changes

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def _logger():
 
 
 LOG = _logger()
-LOG.debug("Starting with log level: %s" % LOG_LEVEL )
+LOG.debug("Starting w log level: %s" % LOG_LEVEL )
 APP = Flask(__name__)
 
 def require_jwt(function):
